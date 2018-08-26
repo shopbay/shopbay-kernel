d19d2648f3c022043e69d7d282c172b3c79a3899
kwlok <kwlok@shopbay.org>
2018-08-26 18:17:03 +0800

    Bug: Set the default uploaded image url to be publicly accessible in class `common.components.actions.CkeditorImageUploadAction`, else image will not be shown across different apps (e.g. access tutorial images from community module)

--- components/actions/CkeditorImageUploadAction.php
+++ components/actions/CkeditorImageUploadAction.php
@@ -13,11 +13,25 @@ class CkeditorImageUploadAction extends CAction
 {
     public $supportMimeType = ['image/gif','image/jpeg','image/png'];
     /**
+     * The domain to host the uploaded image
+     * Default to host domain (where the app domain is calling this action)
+     * TODO Should we have a cdn domain to host all public accessible images
+     * @var string 
+     */
+    public $hostDomain;
+    /**
+     * Set if the uploaded image file is to be accessed using preview mode
+     * Note: preview mode means it is publicly accessible and not protected by authentication
+     * @var string 
+     */
+    public $imagePreview = true;
+    /**
      * The main action that handles the image upload from ckeditor.
      */
     public function run() 
     {
         if(isset($_FILES['upload'])){
+            
             try {
 
                 $upload = CUploadedFile::getInstanceByName('upload');
@@ -38,19 +52,21 @@ class CkeditorImageUploadAction extends CAction
                         }
                         else {
                             if ($upload->saveAs($media->filepath)){
-                                 // Required: anonymous function reference number as explained above.
-                                 $funcNum = $_GET['CKEditorFuncNum'] ;
-                                 // Optional: instance name (might be used to load a specific configuration file or anything else).
-                                 $CKEditor = $_GET['CKEditor'] ;
-                                 // Optional: might be used to provide localized messages.
-                                 $langCode = $_GET['langCode'] ;
-                                 // Check the $_FILES array and save the file. Assign the correct path to a variable ($url).
-                                 $url = $media->getAssetUrl(Yii::app()->urlManager->hostDomain);
-                                 // Usually you will only assign something here if the file could not be uploaded.
-                                 $message = '';
+                                // Required: anonymous function reference number as explained above.
+                                $funcNum = $_GET['CKEditorFuncNum'] ;
+                                // Optional: instance name (might be used to load a specific configuration file or anything else).
+                                $CKEditor = $_GET['CKEditor'];
+                                // Optional: might be used to provide localized messages.
+                                $langCode = $_GET['langCode'];
+                                // Check the $_FILES array and save the file. Assign the correct path to a variable ($url).
+                                if ($this->hostDomain==null)
+                                    $this->hostDomain = Yii::app()->urlManager->hostDomain;
+                                $url = $media->getAssetUrl($this->hostDomain,$this->imagePreview);
+                                // Usually you will only assign something here if the file could not be uploaded.
+                                $message = '';
 
-                                 $output = '<html><body><script type="text/javascript">window.parent.CKEDITOR.tools.callFunction('.$funcNum.', "'.$url.'","'.$message.'");</script></body></html>';
-                                 echo $output;
+                                $output = '<html><body><script type="text/javascript">window.parent.CKEDITOR.tools.callFunction('.$funcNum.', "'.$url.'","'.$message.'");</script></body></html>';
+                                echo $output;
                             }
                             else
                                 throw new CException('Image upload failed');                            
